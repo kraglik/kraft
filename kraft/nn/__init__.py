9192214daaff0d1c50dc904ab32ff9e677eaa23e
Igor Kraglik <kraglik.i.d@gmail.com>
2022-12-26 12:01:14 +0400

    added example of a convolutional neural network

--- kraft/nn/__init__.py
+++ kraft/nn/__init__.py
@@ -1,4 +1,10 @@
 from .module import Module, Parameter
 from .sequential import Sequential
-from .layer import Linear, Conv2d, ConvTranspose2d
+from .layer import (
+    Linear,
+    Conv2d,
+    ConvTranspose2d,
+    MaxPool2d,
+    AvgPool2d,
+)
 from .activation import Sigmoid, ReLU
--- mnist.py
+++ mnist.py
@@ -2,22 +2,41 @@ import kraft
 import kraft.device
 import kraft.optim
 import kraft.autograd.ops
+from kraft import nn
+
+
+class MnistConv(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=5),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4),
+            nn.ReLU(),
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(64, 128),
+            nn.ReLU(),
+            nn.Linear(128, 10),
+            nn.ReLU(),
+        )
+
+        self.softmax = nn.Softmax()
+
+    def forward(self, xs):
+        return xs
 
 
 def main():
     device = kraft.device.get_gpu_device()
 
-    data = kraft.randn([4, 16, 128, 128], dtype=kraft.float32, requires_grad=True, device=device)
 
-    w = kraft.randn([8, 16, 3, 3], dtype=kraft.float32, requires_grad=True, device=device)
-    b = kraft.randn([8], dtype=kraft.float32, requires_grad=True, device=device)
-
-    out = kraft.autograd.ops.conv2d(data, W=w, b=b)
-    out = kraft.autograd.ops.max_pool2d(out, kernel_size=2)
-    out = out.sum()
-    out.backward()
-
-    print(data.grad)
 
 
 if __name__ == "__main__":
